424d9d6cad48f9cc8daf4283c02d2eb21d311beb
Jiheui <developer.jiheuilee@gmail.com>
2024-01-18 11:23:03 +0900

    update missing files

---
+++ 11_Java_Standard_API/src/pkg01_String/Class01_String.java
@@ -0,0 +1,32 @@
+package pkg01_String;
+
+public class Class01_String {
+
+    public static void method4(){
+
+        //equals method
+        //equals    : 같은 문자열이면 true
+        //equalsIgnoreCase : 같은 문자열이면 true 반환
+
+    }
+
+    public static void method5(){
+        //length
+
+        String str = "hello\n";
+
+        System.out.println(str.length());
+    }
+
+    public static void method6(){
+        //charAt : 지정한 인덱스의 문자반환
+        
+
+
+    }
+    public static void main(String[] args) {
+        method5();
+    }
+
+
+}
