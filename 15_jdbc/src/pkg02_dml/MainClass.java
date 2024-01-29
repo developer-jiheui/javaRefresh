0b753f71fd92178c105c26f9fd4658753d85e521
Jiheui <developer.jiheuilee@gmail.com>
2024-01-29 15:41:18 +0900

    String files updated

---
+++ 15_jdbc/src/pkg02_dml/MainClass.java
@@ -0,0 +1,65 @@
+package pkg02_dml;
+
+
+public class MainClass {
+
+    /*
+    *
+    */
+
+//    public static void method1(){
+//
+//
+//
+//
+//
+//        try {//Connection prepared statement
+//
+//            Class.forName("oracle.jdbc.OracleDriver");
+//
+//            //데이터베이스 접속정보
+//            String url = "";
+//
+//            String sql = "INTO SAMPLE_T(SAMEPLE_NP, SAMPLE_CONTENT, SAMPLE_EDITOR, SAMPLE_DT) VALUES(SAMPLE_SEQ.NEXTVAL, '콘텐츠' , '에디터', CURRENT_DATE)";
+//            String ps = "";
+//            //PreparedStatement 객체 생성
+//            ps = con.prepareStatement(sql);
+//
+//            //쿼리문 실행. 삽입된 행의 갯수가 반환됨
+//            int result = ps.executeUpdate();
+//            System.out.println(result + "행이 삽입되었습니다.");
+//
+//
+//        } catch (Exception e) {
+//            assert e != null;
+//            e.printStackTrace();
+//        } finally {
+//            //Connection, PreparedStatement 객체 닫기 (반환)
+//            try{
+//
+//                if(con != null) con.close();
+//                if(ps != null) ps.close;
+//
+//            }catch (Exception e){
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    public static void method2(){
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//
+//            String password = "";
+//
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        } finally {
+//        }
+//
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
